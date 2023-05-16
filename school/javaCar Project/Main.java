594386b54c93b2864e4f0be866a5545da75fcbb5
CupOfTea <100515359+CupOfTea12@users.noreply.github.com>
2023-05-16 08:12:38 +0200

    Update Main.java

--- school/javaCar Project/Main.java
+++ school/javaCar Project/Main.java
@@ -1,12 +1,45 @@
+package Main;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Toyota", "V6", 4, 6, 2022, 5000.5);
-        
-        System.out.println("Znacka: " + car.getZnacka());
-        System.out.println("Motor: " + car.getMotor());
-        System.out.println("Pocet kolies: " + car.getPocetKolies());
-        System.out.println("Pocet svetiel: " + car.getPocetSvetiel());
-        System.out.println("Rok vyroby: " + car.getRokVyroby());
-        System.out.println("Pocet najazdenych kilometrov: " + car.getPocetNajazdenychKilometrov());
+        List<Car> cars = createCarList();
+
+        // Print the list of available cars
+        System.out.println("Available Cars:");
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println((i + 1) + ". " + cars.get(i).getZnacka());
+        }
+
+        // Prompt the user to select a car
+        System.out.print("Select a car (1-" + cars.size() + "): ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        // Check if the choice is valid
+        if (choice >= 1 && choice <= cars.size()) {
+            Car selectedCar = cars.get(choice - 1);
+
+            // Print the parameters for the selected car
+            System.out.println("\nSelected Car Parameters:");
+            System.out.println("Znacka: " + selectedCar.getZnacka());
+            System.out.println("Motor: " + selectedCar.getMotor());
+            System.out.println("Pocet kolies: " + selectedCar.getPocetKolies());
+            System.out.println("Pocet svetiel: " + selectedCar.getPocetSvetiel());
+            System.out.println("Rok vyroby: " + selectedCar.getRokVyroby());
+            System.out.println("Pocet najazdenych kilometrov: " + selectedCar.getPocetNajazdenychKilometrov());
+        } else {
+            System.out.println("Invalid choice.");
+        }
+
+        scanner.close();
     }
-}
+
+    // Method to create a list of cars
+    public static List<Car> createCarList() {
+        List<Car> cars = new ArrayList<>();
+
+        // Add cars to the list
+        cars.add(new Car("Toyota", "V6",
